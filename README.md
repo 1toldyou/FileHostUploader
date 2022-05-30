c14df28797daaeba912cee4b1fd1dd9a4f8ecaee
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-05-29 18:46:32 -0700

    more intelligent

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Utilize their API for direct upload
 ## Sync and Async
 Whether you are creating a simple script or performance-drive application, 
 we offer adapters with both [requests](https://pypi.org/project/requests/) and or [aiohttp](https://pypi.org/project/aiohttp/)
+
+## Setup
+`pip install requirements.txt`
--- synchronous/litterbox.py
+++ synchronous/litterbox.py
@@ -1,5 +1,8 @@
+from typing import Union
 import time
+
 import requests
+import magic
 
 
 def _base_upload(file_name: str, file_body: bytes, file_type: str, expiration_time: str = "1h", cookie: str = "PHPSESSID=hfppk8pkpusimeb9dlrgjbm8ve"):
@@ -25,15 +28,30 @@ def _base_upload(file_name: str, file_body: bytes, file_type: str, expiration_ti
         return None
 
 
-def upload():
-    return _base_upload(file_name="blank_white.png",
-                        file_body=open("../example_file/blank_white.png", 'rb').read(),
-                        file_type="image/png",
-                        expiration_time="1h",
+def upload(file_name: str, file: Union[bytes, str], file_type: str = None, expiration_time: str = "1h"):
+    if isinstance(file, bytes):
+        file_body = file
+    elif isinstance(file, str):
+        file_body = open(file, "rb").read()
+    else:
+        raise FileNotFoundError("you need provide a file to upload")
+    if file_type is None:
+        file_type = magic.from_file(file, mime=True)
+    if not (expiration_time == "1h" or expiration_time == "12h" or expiration_time == "24h" or expiration_time == "72h"):
+        expiration_time = "12h"
+    return _base_upload(file_name=file_name,
+                        file_body=file_body,
+                        file_type=file_type,
+                        expiration_time=expiration_time,
                         cookie="PHPSESSID=hfppk8pkpusimeb9dlrgjbm8ve")
+
+
+# TODO
+def get_cookie():
+    pass
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(upload())
+    print(upload(file_name="blank_white.png", file="../example_file/blank_white.png"))
     print("%s seconds" % (time.time() - start_time))
