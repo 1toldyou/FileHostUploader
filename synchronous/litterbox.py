c6ac29562089c2996f23e5114ec29e9a84d70e00
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-05-29 19:44:50 -0700

    get cookie

--- synchronous/litterbox.py
+++ synchronous/litterbox.py
@@ -5,30 +5,24 @@ import requests
 import magic
 
 
-def _base_upload(file_name: str, file_body: bytes, file_type: str, expiration_time: str = "1h", cookie: str = "PHPSESSID=hfppk8pkpusimeb9dlrgjbm8ve"):
+def _base_upload(file_name: str, file_body: bytes, file_type: str, expiration_time: str, cookie: str, client: requests.Session):
     url = "https://litterbox.catbox.moe/resources/internals/api.php"
 
-    payload = {'time': expiration_time,
-               'reqtype': 'fileupload'}
-    files = {
-        'fileToUpload': (file_name, file_body, file_type)
-    }
+    payload = {'time': expiration_time, 'reqtype': 'fileupload'}
+    files = {'fileToUpload': (file_name, file_body, file_type)}
+    headers = {'Cookie': cookie}
 
-    headers = {
-        'Cookie': cookie
-    }
+    resp = requests.post(url, headers=headers, data=payload, files=files)
+    print(resp.status_code)
+    print(resp.text)
 
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    print(response.status_code)
-    print(response.text)
-
-    if response.status_code == 200:
-        return response.text
+    if resp.status_code == 200:
+        return resp.text
     else:
         return None
 
 
-def upload(file_name: str, file: Union[bytes, str], file_type: str = None, expiration_time: str = "1h"):
+def upload(file_name: str, file: Union[bytes, str], file_type: str = None, expiration_time: str = "1h", cookie: str = "", client: requests.Session = requests.Session()):
     if isinstance(file, bytes):
         file_body = file
     elif isinstance(file, str):
@@ -43,15 +37,23 @@ def upload(file_name: str, file: Union[bytes, str], file_type: str = None, expir
                         file_body=file_body,
                         file_type=file_type,
                         expiration_time=expiration_time,
-                        cookie="PHPSESSID=hfppk8pkpusimeb9dlrgjbm8ve")
+                        cookie=cookie,
+                        client=client)
 
 
-# TODO
-def get_cookie():
-    pass
+def get_cookie(client: requests.Session = requests.Session()):
+    resp = client.get("https://litterbox.catbox.moe/")
+    if resp.status_code == 200:
+        return f"PHPSESSID={list(resp.cookies.values())[0]}"
+    else:
+        return ""
 
 
 if __name__ == "__main__":
     start_time = time.time()
-    print(upload(file_name="blank_white.png", file="../example_file/blank_white.png"))
+    session = requests.Session()
+    print(upload(file_name="blank_white.png",
+                 file="../example_file/blank_white.png",
+                 client=session,
+                 cookie=get_cookie(client=session)))
     print("%s seconds" % (time.time() - start_time))
