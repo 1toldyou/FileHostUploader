75550cc15ec578359082412e714e33db03a4a194
1toldyou <86680163+1toldyou@users.noreply.github.com>
2022-07-19 22:08:24 -0700

    why the package not installing

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ setup(
     author="1toldyou",
     url="https://github.com/1toldyou/FileHostUploader",
     description="all-in-one tool to upload file to various free or low-cost file sharing platform",
-    packages=["file_host_uploader"],
+    packages=[
+        "file_host_uploader",
+        # "file_host_uploader.synchronous"
+    ],
     install_requires=[
         "requests",
         "aiohttp",
